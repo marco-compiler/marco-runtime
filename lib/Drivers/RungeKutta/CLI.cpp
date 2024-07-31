a672cb27eb72895513abdc93b3fc61512b2f1552
Michele Scuttari <michele.scuttari@outlook.com>
2024-07-31 18:34:10 +0200

    Disable clang-format for CLI-related code

--- lib/Drivers/RungeKutta/CLI.cpp
+++ lib/Drivers/RungeKutta/CLI.cpp
@@ -7,25 +7,20 @@ namespace marco::runtime::rungekutta {
 std::string CommandLineOptions::getTitle() const { return "Runge-Kutta"; }
 
 void CommandLineOptions::printCommandLineOptions(std::ostream &os) const {
-  os << "  --time-step=<value>        Set the time step (in seconds). Defaults "
-        "to "
-     << getOptions().timeStep << "." << std::endl;
-
-  os << "  --tolerance=<value>        Set the tolerance for adaptive "
-        "Runge-Kutta methods. If set, the provided time step is used only for "
-        "the first step. Defaults to "
-     << getOptions().tolerance << "." << std::endl;
-
-  os << "  --max-iterations=<value>   Set the maximum number of iterations for "
-        "adaptive Runge-Kutta methods. Defaults to "
-     << getOptions().maxIterations << "." << std::endl;
+  // clang-format off
+  os << "  --time-step=<value>        Set the time step (in seconds). Defaults to " << getOptions().timeStep << "." << std::endl;
+  os << "  --tolerance=<value>        Set the tolerance for adaptive Runge-Kutta methods. If set, the provided time step is used only for the first step. Defaults to " << getOptions().tolerance << "." << std::endl;
+  os << "  --max-iterations=<value>   Set the maximum number of iterations for adaptive Runge-Kutta methods. Defaults to " << getOptions().maxIterations << "." << std::endl;
+  // clang-format on
 }
 
 void CommandLineOptions::parseCommandLineOptions(
     const argh::parser &options) const {
+  // clang-format off
   options("time-step") >> getOptions().timeStep;
   options("tolerance") >> getOptions().tolerance;
   options("max-iterations") >> getOptions().maxIterations;
+  // clang-format on
 }
 } // namespace marco::runtime::rungekutta
 
--- lib/Simulation/CLI.cpp
+++ lib/Simulation/CLI.cpp
@@ -4,39 +4,35 @@
 #include "marco/Runtime/Simulation/Options.h"
 #include <iostream>
 
-namespace marco::runtime::simulation
-{
-  std::string CommandLineOptions::getTitle() const
-  {
-    return "General";
-  }
+namespace marco::runtime::simulation {
+std::string CommandLineOptions::getTitle() const { return "General"; }
 
-  void CommandLineOptions::printCommandLineOptions(
-      std::ostream& os) const
-  {
-    os << "  --debug                          Enable the debug messages." << std::endl;
+void CommandLineOptions::printCommandLineOptions(std::ostream &os) const {
+  // clang-format off
+  os << "  --debug                          Enable the debug messages." << std::endl;
 
-    os << "  --start-time=<value>             Set the start time (in seconds). Defaults to " << getOptions().startTime << "." << std::endl;
-    os << "  --end-time=<value>               Set the end time (in seconds). Defaults to " << getOptions().endTime << "." << std::endl;
-    os << "  --equations-partitioning-factor  Set the amount of equation partitions each thread would process in an ideal scenario where all the equations are independent from each other and have equal computational cost. Defaults to " << getOptions().equationsPartitioningFactor << "." << std::endl;
-    os << "  --scheduler-calibration-runs     Set the amount of sequential and multithreaded executions used to decide the execution policy. Defaults to " << getOptions().schedulerCalibrationRuns << "." << std::endl;
-  }
-
-  void CommandLineOptions::parseCommandLineOptions(
-      const argh::parser& options) const
-  {
-    getOptions().debug = options["debug"];
-
-    options("start-time") >> getOptions().startTime;
-    options("end-time") >> getOptions().endTime;
-    options("equations-partitioning-factor") >> getOptions().equationsPartitioningFactor;
-    options("scheduler-calibration-runs") >> getOptions().schedulerCalibrationRuns;
-  }
-
-  std::unique_ptr<cli::Category> getCLIOptions()
-  {
-    return std::make_unique<CommandLineOptions>();
-  }
+  os << "  --start-time=<value>             Set the start time (in seconds). Defaults to " << getOptions().startTime << "." << std::endl;
+  os << "  --end-time=<value>               Set the end time (in seconds). Defaults to " << getOptions().endTime << "." << std::endl;
+  os << "  --equations-partitioning-factor  Set the amount of equation partitions each thread would process in an ideal scenario where all the equations are independent from each other and have equal computational cost. Defaults to " << getOptions().equationsPartitioningFactor << "." << std::endl;
+  os << "  --scheduler-calibration-runs     Set the amount of sequential and multithreaded executions used to decide the execution policy. Defaults to " << getOptions().schedulerCalibrationRuns << "." << std::endl;
+  // clang-format on
 }
+
+void CommandLineOptions::parseCommandLineOptions(
+    const argh::parser &options) const {
+  // clang-format off
+  getOptions().debug = options["debug"];
+
+  options("start-time") >> getOptions().startTime;
+  options("end-time") >> getOptions().endTime;
+  options("equations-partitioning-factor") >> getOptions().equationsPartitioningFactor;
+  options("scheduler-calibration-runs") >> getOptions().schedulerCalibrationRuns;
+  // clang-format on
+}
+
+std::unique_ptr<cli::Category> getCLIOptions() {
+  return std::make_unique<CommandLineOptions>();
+}
+} // namespace marco::runtime::simulation
 
 #endif // CLI_ENABLE
