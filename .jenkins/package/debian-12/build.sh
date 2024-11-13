8df8fbfe7ce341af277106ee3aa1ea7079e9fa2f
Michele Scuttari <michele.scuttari@outlook.com>
2024-11-13 19:05:38 +0100

    Avoid renaming Debian package

--- .jenkins/package/debian-12/build.sh
+++ .jenkins/package/debian-12/build.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 
+# The path where the project was cloned.
 src_path=$1
+
+# The path where the project was installed.
 install_path=$2
-
-# Create folders.
-mkdir -p marco-runtime/DEBIAN
-mkdir -p marco-runtime/usr/lib/marco-runtime
-
-# Copy the control file.
-cp "${src_path}/.jenkins/package/debian-12/control" marco-runtime/DEBIAN/control
-
-# Copy the libraries.
-cp "${install_path}"/lib/*.a  marco-runtime/usr/lib/marco-runtime
-cp "${install_path}"/lib/*.so marco-runtime/usr/lib/marco-runtime
-chmod +x marco-runtime/usr/lib/marco-runtime/*.so
-
-# Build the package.
-dpkg-deb --build marco-runtime
-
-# Clean the work directory.
-rm -rf marco-runtime
 
 # Extract version and architecture from the control file.
 version=$(grep "^Version:" "${src_path}/.jenkins/package/debian-12/control" | cut -d' ' -f2)
 architecture=$(grep "^Architecture:" "${src_path}/.jenkins/package/debian-12/control" | cut -d' ' -f2)
 
-# Rename the package.
-mv marco-runtime.deb marco-runtime-${version}_${architecture}.deb
+# Create folders.
+package_name=marco-runtime-${version}_${architecture}
+
+mkdir -p "${package_name}"/DEBIAN
+mkdir -p "${package_name}"/usr/lib/marco-runtime
+
+# Copy the control file.
+cp "${src_path}/.jenkins/package/debian-12/control" "${package_name}"/DEBIAN/control
+
+# Copy the libraries.
+cp "${install_path}"/lib/*.a "${package_name}"/usr/lib/marco-runtime
+cp "${install_path}"/lib/*.so "${package_name}"/usr/lib/marco-runtime
+chmod +x "${package_name}"/usr/lib/marco-runtime/*.so
+
+# Build the package.
+dpkg-deb --build "${package_name}"
+
+# Clean the work directory.
+rm -rf "${package_name}"
